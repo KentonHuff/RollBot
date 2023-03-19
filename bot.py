bc10aae3f7c288ed6570fdb7c0562fc962a04431
Kenton Huff <kentonahuff@gmail.com>
2023-03-18 23:05:56 -0500

    Main functionality

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 # bot.py
 import os
-import random
-from parse_message import parse_message
+from parse_message import parse_expression
+import re
+import json
 
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+named_roll_dict = {}
+with open("named_rolls.json") as names_file:
+    named_roll_dict = json.load(names_file)
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -16,17 +21,55 @@ async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
 @client.event
-async def on_error(event, *args, **kwargs):
-    return
-
-@client.event
 async def on_message(message):
-
-    if message.author == client.user or message.channel.name != 'rolls':
+    if message.author == client.user:
+        return
+    if message.content.casefold() == 'help'.casefold():
+        await message.channel.send('helpmsg')
+        return
+    if message.channel.name != 'rolls':
         return
     
-    result = '"'+message.content+'" for '+str(message.author.mention)+':\n'+parse_message(message.content)
+    global named_roll_dict
 
-    await message.channel.send(result)
+    processed_content = message.content
+
+    roll_name_match = re.search(r'^".*"',processed_content)
+    roll_name = None
+
+    if str(message.author.id) in named_roll_dict and message.content in named_roll_dict[str(message.author.id)]:
+        roll_name = message.content
+        processed_content = named_roll_dict[str(message.author.id)][message.content]
+    elif str(message.author.id) in named_roll_dict and len(message.content) >= 2 and message.content[0] == '"' and message.content[-1] == '"' and message.content[1:-1] in named_roll_dict[str(message.author.id)]:
+        roll_name = message.content[1:-1]
+        processed_content = named_roll_dict[str(message.author.id)][message.content[1:-1]]
+    elif roll_name_match:
+        roll_name = roll_name_match.group(0)[1:-1]
+        processed_content = processed_content[:roll_name_match.start()]+processed_content[roll_name_match.end():]
+        if str(message.author.id) not in named_roll_dict:
+            named_roll_dict[str(message.author.id)] = {}
+        named_roll_dict[str(message.author.id)][roll_name] = processed_content
+        with open("named_rolls.json",'w') as names_file:
+            json.dump(named_roll_dict,names_file,indent=4,sort_keys=True)
+    
+    processed_content = ''.join(processed_content.split())
+
+    processed_content = processed_content.replace('\\','')
+    valid_chars_match = re.search(r'^[-\d\.\^\+\*/s_\(\)d]*$',processed_content)
+    if not valid_chars_match:
+        return
+    
+    if roll_name == None:
+        roll_name = message.content
+
+    output_log = roll_name+' for '+str(message.author.mention)+':\n'
+    output_log, expression = parse_expression(output_log, processed_content)
+
+    output_log += expression
+    output_log = re.sub(r'(?<!\\)\*','\*',output_log)
+    output_log += ' = **'+str(eval(expression))+'**'
+
+    await message.channel.send(output_log)
+    return
 
 client.run(TOKEN)
--- parse_message.py
+++ parse_message.py
@@ -1,60 +1,82 @@
 import random
+import re
 
-def roll(mult, die):
-	total = 0
-	str_result = ''
-	for x in range(0, mult):
-		rollVal = random.randint(1, die)
-		total += rollVal
-		str_result += 'd' + str(die) + ': ' + str(rollVal)+'\n'
-	return str_result, total
+def roll(output_log, roll_type, multiplier, dice_type):
+	if multiplier == 0:
+		return output_log, '0'
+	
+	rolls = []
+	for x in range(0, multiplier):
+		rollVal = random.randint(1, dice_type)
+		rolls.append(rollVal)
+		output_log += 'd' + str(dice_type) + ': ' + str(rollVal)+'\n'
+	
+	if len(rolls) == 1:
+		return output_log, str(rolls[0])
+	
+	char_to_agg_func = {
+		'^':'max',
+		's':'sum',
+		'_':'min'
+	}
 
+	replacement = char_to_agg_func[roll_type]+'('+''.join(str(rolls).split())+')'
 
-def parseRoll(string):
-	isNegative = False
-	if string[0] == '-':
-		string = string.replace('-','')
-		isNegative = True
-	
-	multiplier = 1
-	die = 1
-	array = string.split('d')
-	array = [x for x in array if x != '']
-	if len(array) == 1:
-		die = int(array[0])
-	else:
-		multiplier = int(array[0])
-		die = int(array[1])
-	
-	output, roll_total = roll(multiplier, die)
-	if isNegative:
-		return -1 * roll_total
-	else:
-		return output, roll_total
-	
+	return output_log, replacement
 
-def execute(active):
-	active = active.replace(' ','')
-	active = active.replace('-', '+-')
-	array = active.split('+')
-	array = [x for x in array if x != '']
-	
-	total = 0
+def parse_expression(output_log, expression):
+	#[\+-]4
+	expr_match = re.search(r'^[\+-]((\d+)|(\d+\.\d*)|(\d*\.\d+))$',expression)
+	if expr_match:
+		expression = 's1d20'+expression
+		return parse_expression(output_log,expression)
 
-	output = ''
-	
-	for index in range(0, len(array)):
-		if 'd' in array[index]:
-			roll_out, array[index] = parseRoll(array[index])
-			output += roll_out
-			
-		total += int(array[index])
+	#[\^s_]3d12 Full definition
+	expr_match = re.search(r'([\^s_])(\d+)d(\d*[1-9]\d*)',expression)
+	if expr_match:
+		output_log, replacement = roll(output_log,
+				 					roll_type=expr_match.group(1),
+				 					multiplier=int(expr_match.group(2)),
+				 					dice_type=int(expr_match.group(3)))
 		
-		
-	return output+'Result: ' + str(total)
+		expression = expression[:expr_match.start()]+replacement+expression[expr_match.end():]
+		return parse_expression(output_log,expression)
 
-def parse_message(msg):
-	return execute(msg)
 
-#print(eval('min([1,2,3])'))
-#print(str([1,2,3]))
+	#[\^s_]d12
+	expr_match = re.search(r'([\^s_])d(\d*[1-9]\d*)',expression)
+	if expr_match:
+		char_to_num_dice = {
+			'^':'2',
+			's':'1',
+			'_':'2'
+		}
+		expression = expression[:expr_match.start()]+expr_match.group(1)+char_to_num_dice[expr_match.group(1)]+'d'+expr_match.group(2)+expression[expr_match.end():]
+		return parse_expression(output_log,expression)
+
+	#[\^s_]
+	expr_match = re.search(r'[\^_]|s(?!u)',expression)
+	if expr_match:
+		char_to_repl = {
+			'^':'^2d20',
+			's':'s1d20',
+			'_':'_2d20'
+		}
+		expression = expression[:expr_match.start()]+char_to_repl[expr_match.group(0)]+expression[expr_match.end():]
+		return parse_expression(output_log,expression)
+
+	#3d10
+	expr_match = re.search(r'\d+d\d*[1-9]\d*',expression)
+	if expr_match:
+		expression = expression[:expr_match.start()]+'s'+expr_match.group(0)+expression[expr_match.end():]
+		return parse_expression(output_log,expression)
+	
+	#d4
+	expr_match = re.search(r'd\d*[1-9]\d*',expression)
+	if expr_match:
+		expression = expression[:expr_match.start()]+'s1'+expr_match.group(0)+expression[expr_match.end():]
+		return parse_expression(output_log,expression)
+
+
+
+	return output_log, expression
